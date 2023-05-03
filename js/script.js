4dc2ee813e3d06b88d174a3f5d96eca9aa3befcc
Marcos Meneghetti <meneghettimarcos@outlook.com>
2023-05-03 10:37:32 +0200

    MODIFICATION final comments js file and winner background

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 'use strict';
+
+//global variables
+
 //players
 const player1 = document.querySelector('.player__1');
 const player2 = document.querySelector('.player__2');
@@ -45,51 +48,81 @@ const switchPlayer = () => {
 let active = 1;
 
 btnNew.addEventListener('click', () => {
+  //the game start
   playing = true;
 
   activePlayer = 1;
-  document.querySelector('.player__1').classList.add('active');
-  document.querySelector('.player__2').classList.remove('active');
-  document.querySelector('.player__1').classList.remove('winner');
-  document.querySelector('.player__2').classList.remove('winner');
+
+  //reset values of game
+  player1.classList.add('active');
+  player2.classList.remove('active');
+
+  player1.classList.remove('winner');
+  player2.classList.remove('winner');
+
   scorePlayers[0] = 0;
   scorePlayers[1] = 0;
-  document.querySelector('.player__1__score').textContent = 0;
-  document.querySelector('.player__2__score').textContent = 0;
+
+  scorePlayer1.textContent = 0;
+  scorePlayer2.textContent = 0;
+
+  currentPlayer1.textContent = 0;
+  currentPlayer2.textContent = 0;
+
   diceDiv.classList.add('dice--grayscale');
   diceImg.src = `./assets/images/dice_1.png`;
 });
 
 btnRoll.addEventListener('click', () => {
+  //if game is true
   if (playing) {
     diceImg.classList.remove('hidden');
     diceDiv.classList.remove('dice--grayscale');
     diceImg.classList.toggle('dice--rotation');
+
+    //random number for dice
     const diceNumber = Math.trunc(Math.random() * 6) + 1;
+
+    //display dice image based on random number
     diceImg.src = `./assets/images/dice_${diceNumber}.png`;
+
     if (diceNumber !== 1) {
+      //current score from dice
       currentScore += diceNumber;
       document.querySelector(`.current__${activePlayer}`).textContent =
         currentScore;
     } else {
+      // call switch player function
       switchPlayer();
     }
   }
 });
 
 btnHold.addEventListener('click', () => {
+  //if game is true
   if (playing) {
+    //add current score to global score
     scorePlayers[activePlayer - 1] += currentScore;
+
+    //update the active player score
     document.querySelector(`.player__${activePlayer}__score`).textContent =
       scorePlayers[activePlayer - 1];
+
+    //check if player won the game
     if (scorePlayers[activePlayer - 1] >= 20) {
+      //game is finished
       playing = false;
+
+      //winner text
       diceImg.classList.add('hidden');
       document.querySelector(`.player__${activePlayer}__score`).textContent =
         'Winner!';
+
+      //lost text
       document
         .querySelector(`.player__${activePlayer}`)
         .classList.add('winner');
+
       switchPlayer();
 
       document.querySelector(`.player__${activePlayer}__score`).textContent =
